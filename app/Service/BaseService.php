72e91a7ee2cd944adadf4bb3a1ee3f6184b49f79
Mihail Denev <denev@iaxus.com>
2022-03-07 20:14:05 +0200

    Add task skeleton

---
+++ app/Service/BaseService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+
+class BaseService {
+
+    private $session = null;
+
+    public function __construct() {
+        $this->login();
+    }
+
+    public function __desctuct() {
+        $this->logout();
+    }
+
+    public function getSessionId() {
+        return $this->session;
+    }
+
+    public function setSessionId($sessionId) {
+        $this->session = $sessionId;
+        return true;
+    }
+
+    private function login() {
+        //TODO get credentials from environment
+        //TODO implement login functionality
+    }
+    private function logout() {
+        //TODO implement logout functionality
+    }
+
+}
